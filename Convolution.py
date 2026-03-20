40f06b8a738da7679e96233563261fd05eeb135e
Elana <elanafirley@gmail.com>
2026-03-20 11:43:37 +0100

    début squelette

---
+++ Convolution.py
@@ -0,0 +1,12 @@
+class Convolution():
+    def __init__(self,nb_filtres,taille_filtre):
+        self.nb_filtres = nb_filtres
+        self.taille_filtre= taille_filtre
+        pass
+
+    def convolution(self):
+        pass
+    def activation(self):
+        pass
+    def pooling(self):
+        pass
