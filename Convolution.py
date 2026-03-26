3b97a1abb9ee2fa15375ea98560146183fba4c21
Elana <elanafirley@gmail.com>
2026-03-26 10:45:52 +0100

    convolution

--- Convolution.py
+++ Convolution.py
@@ -13,18 +13,50 @@ class Convolution():
         self.pool_size = pool_size
         pass
 
-    def convolution(self,padding,stride,nb_couches,nb_filtres,kernelsize,image):
+    def convolution(self, padding, stride, nb_couches, nb_filtres, kernelsize, image, biais):
         """
-        Applique un filtre sur l'image
-        :param padding: nb de pixels ajoutés sur le bord de l'image (soit 0 ou copie du pixel voisin)
-        :param stride: de combien on décale
-        :param nb_couches: nb couches de convolution
-        :param nb_filtres:
-        :param kernelsize: taille du noyau
-        :return: un nouvelle image de taille réduite ou non
-        """
+                Applique un filtre sur l'image
+                :param padding: nb de pixels ajoutés sur le bord de l'image (soit 0 ou copie du pixel voisin)
+                :param stride: de combien on décale
+                :param nb_couches: nb couches de convolution
+                :param nb_filtres:
+                :param kernelsize: taille du noyau
+                :return: un nouvelle image de taille réduite ou non
+                """
 
-        pass
+        # on ajoute les cases autour de l'image
+        if padding > 0:
+            image_ajout_pads = np.pad(image, ((padding, padding), (padding, padding)), mode='constant')
+        else:
+            image_ajout_pads = image
+
+        # on récupère les dimensions de l'images avec les cases en plus
+        image_h, image_l = image_ajout_pads.shape
+
+        # on calcule la taille de l'image de sortie
+        sortie_h = (image_h - kernelsize) // stride + 1
+        sortie_l = (image_l - kernelsize) // stride + 1
+
+        filtres = np.random.randn(nb_filtres, kernelsize, kernelsize)
+        # on initialise la matrice de sortie avec les biais
+        sortie = np.full((sortie_h, sortie_l), biais)
+
+        for f in range(nb_filtres):
+            for y in range(sortie_h):
+                for x in range(sortie_l):
+                    #on délimite la zone qu'on va regarder
+                    y_start = y * stride
+                    y_end = y_start + kernelsize
+                    x_start = x * stride
+                    x_end = x_start + kernelsize
+
+                    #on en fait une matrice à part entière
+                    fenetre = image_ajout_pads[y_start:y_end, x_start, x_end]
+                    sortie[f, y, x] += np.sum(fenetre * filtres[f])
+
+        return sortie
+
+
 
     def activation(self,image,type_fonction = "ReLu"):
         """
